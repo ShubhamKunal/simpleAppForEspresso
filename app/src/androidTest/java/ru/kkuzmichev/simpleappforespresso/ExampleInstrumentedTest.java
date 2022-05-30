2bd530cb8f591bccfbbe8dc0a920a6374ed9b621
ShubhamKunal <shubhamkunal19@gmail.com>
2022-05-30 13:32:32 +0530

    Lab 1 done

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -1,10 +1,18 @@
 package ru.kkuzmichev.simpleappforespresso;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
 import android.content.Context;
 
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +25,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule rule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    // Positive test
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
+    public void useAppContext() { //checks package name
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
+    }
+
+    // Negative test
+    @Test
+    public void testFloatingButton() { // clickable and if something is displayed
+        onView(ViewMatchers.withId(R.id.fab)).perform(click());
+        onView(ViewMatchers.withId(R.id.nav_slideshow)).check(matches(isDisplayed()));
+
     }
 }
